babc32709da5b03e9c56e70dae0c214281da6e0d
Alex Orellana <lexorel88@gmail.com>
2025-09-03 21:44:36 -0400

    deployass

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: false,
+  //production: false,
   //apiUrl: 'http://localhost:8080',
-  apiUrl: 'http://localhost:8000/api',
+  //apiUrl: 'http://localhost:8000/api',
+  production: true,
+  apiUrl: 'https://alex-orellana-dev-xyz.duckdns.org/api',
 };
