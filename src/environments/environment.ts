26dd95c706391531899599517c70a88f1f11079e
Alex Orellana <lexorel88@gmail.com>
2025-09-03 22:50:01 -0400

    dep 123

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  //production: false,
+  production: false,
   //apiUrl: 'http://localhost:8080',
-  //apiUrl: 'http://localhost:8000/api',
-  production: true,
-  apiUrl: 'https://alex-orellana-dev-xyz.duckdns.org/api',
+  apiUrl: 'http://localhost:8000/api',
+  //production: true,
+  //apiUrl: 'https://alex-orellana-dev-xyz.duckdns.org/api',
 };
