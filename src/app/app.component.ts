5d24cd4a4fc6791714f2e672b173a6e938cde2a3
Alex Orellana <lexorel88@gmail.com>
2025-09-03 21:29:45 -0400

    deploy d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'gestion_documental_frontend';
+  constructor() {
+    console.log('Environment:', environment);
+    console.log('API URL:', environment.apiUrl);
+  }
 }
